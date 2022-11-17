f14edfee9cd97a60ff9efc0c9f790d9400732de8
Shubha Pratim Biswas <shubhapratimbiswas@gmail.com>
2022-11-17 13:14:03 +0530

    Add files via upload

---
+++ installmiku.sh
@@ -0,0 +1,20 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install bash
+sudo apt-get install libwebp-dev
+sudo apt install git
+sudo apt install nodejs -y
+sudo apt install ffmpeg -y
+sudo apt install wget
+sudo apt install npm
+sudo apt install imagemagick
+git clone https://github.com/FantoX001/Miku-MD
+npm install --global yarn
+yarn add sharp
+sudo apt install curl
+curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs
+yarn add sharp
+cd Miku-MD
+npm i
+npm start
